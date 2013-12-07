3613c8466a8738464ddad984ecf71023ad76175e
mainglis <maryashton.inglis@gmail.com>
2013-12-06 22:45:27 -0500

    trying to commit

--- js/volunteer.js
+++ js/volunteer.js
@@ -11,7 +11,10 @@ So on page load, save them in a variable, then default to the ship form
 // Save
 
 // MINE: ship - individual and instant - group 
-$('textarea[name=message').html("You can put a message here.");
+$('textarea[name=message-skills').html("Experience in a soup kitchen? With web development? Event marketing? Generally great people skills? Really awesome baker?");
+$('textarea[name=message-reason').html("Personal experience? Curiosity? Spare time?");
+$('textarea[name=message-days').html("Please list the days and times that you are available.");
+
 var individual    = $('#customize_individual');
 var group = $('#customize_group');
 
@@ -33,45 +36,23 @@ $('input[name=type').change(function() {
 
 });
 
-//Scrolling individual
+//Scrolling to customize individual
 
-$('html, body').animate({scrollTop:$('#individual-wrapper').position().top}, 'slow');
+$('html, body').animate({scrollTop:$('#individual-wrapper').offset().top
+}, 1000);
 
 $("#individual-wrapper").click(function(){
-   $('html, body').animate({scrollTop:$('#customize_individual').position().top}, 'slow');
+   $('html, body').animate({scrollTop:$('#customize_individual').offset().top
+}, 1000);
 });
 
-//Scrolling group
+//Scrolling to customize group
 
 $('html, body').animate({scrollTop:$('#group-wrapper').position().top}, 'slow');
 
 $("#group-wrapper").click(function(){
 $('html, body').animate({scrollTop:$('#customize_group').position().top}, 'slow');
 });
-
-// If you want a <button/> with an id of myButton that when clicked will cause you to scroll to a <div/> with and id of myDiv over the course of half a second:
-
- // $('#individual').click(function(){
- //     $('html, body').animate({
- //         scrollTop: $( $.attr(this, '#customize') ).offset().top
- //     }, 500);
- //    return false;
- // });
-
-// $('#individual').click(function() {
-//    //optionally remove the 500 (which is time in milliseconds) of the
-//    //scrolling animation to remove the animation and make it instant
-//    $.scrollTo($('#customize'), 500);
-// });
-
-// function scrollToAnchor(aid){
-//     var aTag = $("a[name='"+ aid +"']");
-//     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-// }
-
-// $("#individual").click(function() {
-//    scrollToAnchor('#customize');
-// });
 
 //Buttons
 $(function() {
@@ -82,6 +63,7 @@ $(function() {
       });
   });
 
+//Date picker
 $(function() {
     $( "#from" ).datepicker({
       defaultDate: "+1w",
@@ -100,3 +82,70 @@ $(function() {
       }
     });
   });
+
+// Separate attempt
+function goto(id, t){
+    //animate to the div id.
+    $(".contentbox-wrapper").animate({"left": -($(id).position().left)}, 600);
+ 
+    // remove "active" class from all links inside #nav
+    $('#nav a').removeClass('active');
+ 
+    // add active class to the current link
+    $(t).addClass('active');
+}
+function goto(id, t){
+    //animate to the div id.
+    $(".contentbox-wrapper").animate({"left": -($(id).position().left)}, 600);
+ 
+    // remove "active" class from all links inside #nav
+    $('#nav a').removeClass('active');
+ 
+    // add active class to the current link
+    $(t).addClass('active');
+}
+
+//Activity menu selection
+ 
+// $('textarea[name=message').html("You can put a message here.");
+// var sk    = $('#customize_sk');
+// var ct = $('#customize_ct');
+
+// // Put in individual
+// $('#type_act_container').html(sk);
+
+// // Listener for type change
+// $('input[name=type').change(function() {
+
+//  	var type = $(this).val();
+
+//  	if(type == 'sk') {
+//  		$('#type_container').html(sk);
+//  	}
+//  	else {
+//  		$('#type_container').html(ct);
+//  		$('#customize_ct').show();
+//  	}
+
+//  });
+
+//Scrolling to the different types of activities 
+
+$('html, body').animate({scrollTop:$('#sk-wrapper').position().top}, 'slow');
+
+$("#sk-wrapper").click(function(){
+$('html, body').animate({scrollTop:$('#customize_sk').position().top}, 'slow');
+});
+
+//OLD Scrolling horizontally for activity descriptions
+// $('html, body').animate({scrollTop:$('#sk-wrapper').position().top}, 'slow');
+
+// $("#sk-wrapper").click(function(){
+// $('html, body').animate({"left": -($('#customize_sk').position().left}, 600) ;
+// });
+
+// $('html, body').animate({scrollTop:$('#ct-wrapper').position().top}, 'slow');
+
+// $("#ct-wrapper").click(function(){
+// $('html, body').animate({"left": -($('#customize_ct').position().left}, 600) ;
+// });
